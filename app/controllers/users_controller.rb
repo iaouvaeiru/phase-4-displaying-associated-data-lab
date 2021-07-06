4f3ce8b3c6ca17e9e99f4c9ccab0c6d4a5ad9278
Leon Fan <leonkind@gmail.com>
2021-07-06 02:07:23 -0500

    commit tree 00c8565b576769337a292faa0e1362eede6b133f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include: :items
+        else
+            render json: {error: '404 user not found'}
+        end
+    end
 end
